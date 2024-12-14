ee6cd3d4b4bc18deafd1b52c5101c5fd1d17ec8b
= <jmbalanzar@gmail.com>
2024-12-14 14:37:43 -0600

    test: testing tauri capabilities

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,57 @@
+use tauri::{AppHandle, Emitter};
+use serde::{Serialize, Deserialize};
+use tauri::Listener;
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+struct DownloadStarted<'a>{
+    url: &'a str,
+    download_id: usize,
+    content_length: usize,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+struct DownloadProgress{
+    download_id: usize,
+    chunk_length: usize,
+}
+
+
+#[derive(Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+struct DownloadFinished{
+    download_id: usize,
+}
+
+
+#[tauri::command]
+fn download(app:AppHandle, url:String) -> String {
+    let content_length = 1000;
+    let download_id = 1;
+    
+    app.emit("download-started", DownloadStarted {
+        url: &url,
+        download_id,
+        content_length,
+    }).unwrap();
+    
+    for chunk_length in [1, 15, 50, 80, 100] {
+        app.emit("download-progress", DownloadProgress {
+            download_id,
+            chunk_length,
+        }).unwrap();
+    }
+    app.emit("download-completed", DownloadFinished { download_id }).unwrap();
+    
+    url.to_string()
+}
+
+// #[tauri::command]
+// fn download(url: &str) -> String {
+//     url.to_string()
+// }
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +62,18 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            app.listen("download-progress", |event| {
+                if let Ok(payload) = serde_json::from_str::<DownloadProgress>(&event.payload()) {
+                    println!("downloading {}", payload.chunk_length)
+                }
+            });
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            download
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+const { once } = window.__TAURI__.event;
+const { getCurrentWebviewWindow } = window.__TAURI__.webviewWindow;
+const tauri = window.__TAURI__;
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  let greetInputEl = document.querySelector("#greet-input");
+  let greetMsgEl = document.querySelector("#greet-msg");
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
+
+once('download-started', (event) => {
+  console.log('Download started:', event.payload);
+})
+
+
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
   });
+
+  document.querySelector("#download-file").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    let urlInputEl = document.querySelector("#url-input");
+    let url = urlInputEl.value;
+    let response = await invoke("download", { url: url });
+    const view = getCurrentWebviewWindow();
+    view.once('download-completed', (event) => {
+      localStorage.setItem('downloaded', JSON.stringify(event.payload));
+    })
+    console.log(response);
+    console.log(tauri);
+
+  });
+
 });
